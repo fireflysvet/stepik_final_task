fead3e470aa45663194ab5cc5064957db14e497a
Sviatlana Lukashonak <fireflysvet@gmail.com>
2024-06-28 14:36:34 +0300

    book name and price in a separate method

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,19 +7,23 @@ class ProductPage(BasePage):
         add_to_cart_link = self.browser.find_element(*ProductPageLocators.ADD_TO_CART)
         add_to_cart_link.click()
 
+    def book_name(self):
+        return self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+
+    def book_price(self):
+        return self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+
     def should_be_successful_add_alert(self):
         assert self.is_element_present(*ProductPageLocators.SUCCESSFUL_ALERT), "There are no successful alert"
 
-    def should_be_book_name_in_alert(self):
-        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+    def should_be_book_name_in_alert(self, name):
         successful_alerts = [i.text for i in self.browser.find_elements(*ProductPageLocators.SUCCESSFUL_ALERT)]
-        assert any([book_name in i for i in successful_alerts]), "There is no book name in alert"
+        assert any([name in i for i in successful_alerts]), "There is no book name in alert"
 
     def should_be_alert_with_cart_price(self):
         assert self.is_element_present(*ProductPageLocators.CART_ALERT), "There is no alert with cart price"
 
-    def should_be_correct_price_in_alert(self):
-        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+    def should_be_correct_price_in_alert(self, price):
         cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE_ALERT).text
-        assert book_price == cart_price, "The cart price isn't equal to book price"
+        assert price == cart_price, "The cart price isn't equal to book price"
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,14 @@
 from .pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
+    book_name = page.book_name()
+    book_price = page.book_price()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_successful_add_alert()
-    page.should_be_book_name_in_alert()
+    page.should_be_book_name_in_alert(book_name)
     page.should_be_alert_with_cart_price()
-    page.should_be_correct_price_in_alert()
+    page.should_be_correct_price_in_alert(book_price)
